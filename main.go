d3a27c63dd316331da93aea3633736bd7ba91f20
LeCHAUVEaLaCape <79144206+LeCHAUVEaLaCape@users.noreply.github.com>
2021-05-12 12:42:21 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -1,44 +1,79 @@
 package main
 
 import (
-	"errors"
+	"encoding/json"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
-	grab "./data"
 )
 
-var data []grab.MyArtistFull
+var tpl *template.Template
 
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	err := grab.GetData()
-	if err != nil {
-		errors.New("Error by get data")
-	}
-	if !(len(data) != 0) {
-		data = Datass()
-	}
-
-	tmpl, err := template.ParseFiles("index.html")
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-	if err := tmpl.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
+type Locations struct {
+	Locations []string `json:"locations"`
+}
+type ConcertDates struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+type Artists struct {
+	Id                int          `json:"id"`
+	Image             string       `json:"image"`
+	Name              string       `json:"name"`
+	Members           []string     `json:"members"`
+	CreationDate      int          `json:"creationDate"`
+	Firstalbum        string       `json:"firstAlbum"`
+	Locations         string       `json:"locations"`
+	ConcertDates      string       `json:"concertDates"`
+	Relations         string       `json:"relations"`
+	LocationsValue    Locations    `json:"locationsValues"`
+	ConcertDatesValue ConcertDates `json:"concertDatesValue"`
 }
 
-func Datass() []grab.MyArtistFull {
-	return grab.ArtistsFull
+func init() {
+	tpl = template.Must(template.ParseGlob("*.html"))
+}
+func getAPI(lien string, adresseArtists *[]Artists) {
+	// Prend la donnée
+	response, err := http.Get(lien)
+	if err != nil {
+		log.Println(err)
+	}
+	tmp, _ := ioutil.ReadAll(response.Body)
+	err = json.Unmarshal(tmp, &adresseArtists)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func getAPIValue(lien string) []byte {
+	response, err := http.Get(lien)
+	if err != nil {
+		log.Println(err)
+	}
+	tmp, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	return tmp
+}
+func getValues(data *[]Artists) {
+	for i := range *data {
+		err := json.Unmarshal(getAPIValue((*data)[i].Locations), &(*data)[i].LocationsValue) //marche
+		if err != nil {
+			log.Println(err)
+		}
+		err = json.Unmarshal(getAPIValue((*data)[i].ConcertDates), &(*data)[i].ConcertDatesValue)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
 
+// Error Code à ajouter
 func main() {
-
+	// Créer le fichier des erreurs
 	file, err := os.OpenFile("Errors.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -46,8 +81,17 @@ func main() {
 	defer file.Close()
 	log.SetOutput(file)
 
-	http.HandleFunc("/", mainPage)
+	var data []Artists
+	getAPI("https://groupietrackers.herokuapp.com/api/artists", &data)
+	getValues(&data)
 
+	// Partie serveur
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err = tpl.ExecuteTemplate(w, "index.html", data)
+		if err != nil {
+			log.Println(err)
+		}
+	})
 	fs := http.FileServer(http.Dir("CSS")) //Allow the link of the CSS to the html file
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", fs))
 	err = http.ListenAndServe(":8080", nil)
